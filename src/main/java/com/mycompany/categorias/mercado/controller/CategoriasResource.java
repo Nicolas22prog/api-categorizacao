d9b5449731462e9f53a771f8afeef799ec783ec3
Nicolas <nicolasmoneteiro8@gmail.com>
2025-06-04 13:39:01 -0300

    formatação de codigo

--- src/main/java/com/mycompany/categorias/mercado/controller/CategoriasResource.java
+++ src/main/java/com/mycompany/categorias/mercado/controller/CategoriasResource.java
@@ -11,7 +11,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
